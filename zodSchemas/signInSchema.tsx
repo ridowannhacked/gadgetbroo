ff204c9d2e9a959cc3f22648f43fc18af47b501e
NOTHING-R <kalilinux.nothing@gmail.com>
2026-07-29 12:32:08 +0600

    zod schemas

--- zodSchemas/signInSchema.tsx
+++ zodSchemas/signInSchema.tsx
@@ -1,8 +1,7 @@
 import { z } from 'zod'
 
 export const signInSchema = z.object({
-  email: z.email({ message: "Please enter a valid email address" })
-    .regex(/^[A-Za-z0-9._%+-]+@gmail\.com$/i, { message: "Only Gmail addresses are allowed", }),
+  email: z.email({ message: "Please enter a valid email address" }),
   password: z.string().min(1, { message: "Password is required" }),
   rememberMe: z.boolean().optional(),
 });
