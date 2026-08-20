8a21401fe10bfd65a195194fcd42d4e6e9a03d75
NOTHING-R <kalilinux.nothing@gmail.com>
2026-08-20 11:54:54 +0600

    commit

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-process.env.PORT = process.env.PORT || '2222';
+process.env.PORT = process.env.PORT || '2323';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require('./.next/standalone/server.js');
